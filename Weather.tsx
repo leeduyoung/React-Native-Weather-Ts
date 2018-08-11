e65edd48b03fee330d9f5b82971c74594c1fd278
이두영 <leeduyoung20@naver.com>
2018-08-11 11:11:14 +0900

    날씨 앱 완성

--- Weather.tsx
+++ Weather.tsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, Button, Image, ImageBackground } from 'react-native';
 import Forecast from './Forecast';
 import OpenWeatherMap from './services/OpenWeatherMap';
 
@@ -16,7 +16,6 @@ export default class Weather extends Component {
 
   // _handleTextChange = (event: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
   _handleTextChange = (event: any) => {
-    console.log('_handleTextChange evnet: ', event);
     let zip = event.nativeEvent.text;
     OpenWeatherMap.fetchForecast(zip).then(forecast => {
       console.log(forecast);
@@ -26,7 +25,7 @@ export default class Weather extends Component {
 
   render() {
     let content = null;
-    if(this.state.forecast !== null) {
+    if (this.state.forecast !== null) {
       content = (
         <Forecast
           main={this.state.forecast.main}
@@ -36,52 +35,63 @@ export default class Weather extends Component {
       )
     }
     return (
-      <View style={styles.container}>
-        {/* <Image
-          source={require("./assets/imgs/flowers.png")}
-          resizeMode="cover",
-          style={styles.backdrop}>
-          <View style={styles.overlay}>
-            <View style={styles.row}>
-              <Text style={styles.mainText}>
-                Current weather for
-              </Text>
-              <View style={styles.zipContainer}>
-                <TextInput style={styles.input} onSubmitEditing={this._handleTextChange} />
-              </View>
+      <ImageBackground source={require("./assets/imgs/flowers.png")} resizeMode="cover" style={styles.backdrop} >
+        <View style={styles.overlay}>
+          <View style={styles.row}>
+            <Text style={styles.mainText}>
+              Current weather for 
+            </Text>
+            <View style={styles.zipContainer}>
+              <TextInput style={[styles.zipCode, styles.mainText]} onSubmitEditing={this._handleTextChange} underlineColorAndroid="transparent"/>
+              {/* <TextInput style={{ color:"white", flex: 1, width: 100, paddingLeft: 12, borderBottomColor: "#DDDDDD" }} onSubmitEditing={this._handleTextChange} underlineColorAndroid="transparent"/> */}
             </View>
-
           </View>
-
-        </Image> */}
-        <Text style={styles.welcome}>
-          You input {this.state.zip}
-        </Text>
-        {content}
-        <TextInput style={styles.input} onSubmitEditing={event => this._handleTextChange(event)} />
-      </View>
+          {content}
+        </View>
+      </ImageBackground>
     )
   }
 }
 
+const baseFontSize = 16;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#666666',
+    alignItems: "center",
+    paddingTop: 30,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  backdrop: {
+    flex: 1,
+    flexDirection: "column",
   },
-  input: {
-    fontSize: 20,
-    borderWidth: 2,
-    padding: 2,
-    height: 40,
-    width: 100,
-    textAlign: "center",
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: "#000000",
+    opacity: 0.5,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignItems: "flex-start",
+    padding: 30,
+  },
+  zipContainer: {
+    height: 26,
+    borderBottomColor: "#DDDDDD",
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3,
+  },
+  zipCode: {
+    flex: 1,
+    flexBasis: 1,
+    width: 50,
+    height: 26,
+  },
+  mainText: {
+    fontSize: baseFontSize, color: "#FFFFFF"
   },
 });
